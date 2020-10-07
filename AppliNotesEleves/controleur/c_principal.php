2cf88a47cc46ef6a6fb910679e4ea4d9707e27f8
Lallapalooza <poubelle.g8@mail.com>
2020-10-07 09:04:46 +0200

    modif c_principale

--- AppliNotesEleves/controleur/c_principal.php
+++ AppliNotesEleves/controleur/c_principal.php
@@ -4,9 +4,7 @@ if (isset($_REQUEST['uc']))
 	switch ($_REQUEST['uc'])
 	{
 		case 'auth' : {  include "c_authentification.php" ; break ;} 
-		case 'production' : {  include "c_production.php" ; break ;} 	
-        case 'magasin' : {  include "c_magasinier.php" ; break ;}
-		case 'visite' : {  include "c_visiteur.php" ; break ;}
+		case 'saisieNote' : {  include "c_saisieNote.php" ; break ;} 	
 	}
 }
 ?>
