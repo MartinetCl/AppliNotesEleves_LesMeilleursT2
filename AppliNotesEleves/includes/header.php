a89dd1846d6e786e59ec283d13163e88ef4af3d8
martinec <martinec@domadj.fr>
2020-10-07 09:05:42 +0200

    Update header.php

--- AppliNotesEleves/includes/header.php
+++ AppliNotesEleves/includes/header.php
@@ -2,44 +2,30 @@
 <html lang="fr">
 <head>
 <meta charset= "UTF-8"/>
-<title>GSB Lot</title>
+<title>Notes Elèves</title>
 <link href="includes\css\style.css" rel="stylesheet" type="text/css" />
 </head>
 <nav>
-<img src="includes\images\logoGSB.png" width="150" height="auto">
 <?php 
 if(isset($_SESSION['idTypeUtilisateur'])){
-echo '<a href = "index.php?uc=decnx"><input id = "bt_deco" type = "button" value = "Deconnexion" ></a>';
+echo '<a href = "index.php?uc=decnx" id = "bt_deco"><input type = "button" value = "Deconnexion" ></a>';
+echo '<div style = "clear : both"></div>';
 }
 ?>
 <ul id= "menu">
 <?php 
 if (isset($_REQUEST['uc']))
 {	
-	if ($_REQUEST['uc'] == 'magasin')
+	if ($_REQUEST['uc'] == '')
 	{
-		echo '    
-		<li><a href = "index.php?uc=magasin&action=renseignement">Renseignement</a></li>
-		<li><a>Consulter</a>
-			<ul>
-				<li><a href = "index.php?uc=magasin&action=stock">En stock</a></li>
-				<li><a href = "index.php?uc=magasin&action=sortis">Sortis</a></li>
-			</ul>
-		</li>
 		';
 	}
-	else if ($_REQUEST['uc'] == 'production')
+	else if ($_REQUEST['uc'] == '')
 	{
-		echo '
-		<li><a href = "index.php?uc=production&action=enregistrer">Enregistrement</a></li>
-		<li><a href = "index.php?uc=production&action=consulter">Consultation</a></li>
 		';
 	}
-	else if ($_REQUEST['uc'] == 'visite')
+	else if ($_REQUEST['uc'] == '')
 	{
-		echo '
-		<li><a href = "index.php?uc=visite&action=depot">Depot echantillon</a></li>
-		<li><a href = "index.php?uc=visite&action=consultation">Consultation</a></li>
 		';
 	}
 }
