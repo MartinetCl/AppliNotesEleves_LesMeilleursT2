9be55222c9a02c3560f25b2d8b7ec108b4f6900a
martinec <martinec@domadj.fr>
2020-10-07 09:26:37 +0200

    Update c_principal.php

--- AppliNotesEleves/controleur/c_principal.php
+++ AppliNotesEleves/controleur/c_principal.php
@@ -5,6 +5,7 @@ if (isset($_REQUEST['uc']))
 	{
 		case 'auth' : {  include "c_authentification.php" ; break ;} 
 		case 'saisieNote' : {  include "c_saisieNote.php" ; break ;}
+		case 'consulterNote' : { include "c_conculterNote.php" ; break; }
 	}
 }
 ?>
