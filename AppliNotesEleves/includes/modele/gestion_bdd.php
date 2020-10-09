27c6f2c6878c1b4aa5f8b62ca16e54a96dcef9bb
martinec <martinec@domadj.fr>
2020-10-09 11:19:56 +0200

    Update gestion_bdd.php

--- AppliNotesEleves/includes/modele/gestion_bdd.php
+++ AppliNotesEleves/includes/modele/gestion_bdd.php
@@ -8,9 +8,55 @@ function connect($id, $mdp){
 	$exec=$bdd->prepare($sql);
     $exec->execute();
 	$curseur=$exec->fetch();
+	$_SESSION['idTypeUtilisateur']=$curseur['idTypeUtilisateur'] ;
+	$_SESSION['idUtilisateur'] = $curseur['idUtilisateur'];
 	return $curseur;
 }
 
+function getMoyenneMatiere($matiere){
+	require "connexion.php";
+	$sql = " ";
+	$exec=$bdd->prepare($sql);
+    $exec->execute();
+    $curseur=$exec->fetchall();
+    return $curseur;
+}
+
+function getLesNotes($matiere){
+	require "connexion.php";
+	$sql = " ";
+	$exec=$bdd->prepare($sql);
+    $exec->execute();
+    $curseur=$exec->fetchall();
+    return $curseur;
+}
+
+function getTrimestre(){
+	require "connexion.php";
+	$sql = " ";
+	$exec=$bdd->prepare($sql);
+    $exec->execute();
+    $curseur=$exec->fetchall();
+    return $curseur;
+}
+
+function getLesMatieres(){
+	require "connexion.php";
+	$sql = " ";
+	$exec=$bdd->prepare($sql);
+    $exec->execute();
+    $curseur=$exec->fetchall();
+    return $curseur;
+}
+
+function getMoyenneGlobale($idUtilisateur){
+	require "connexion.php";
+	$sql = " ";
+	$exec=$bdd->prepare($sql);
+    $exec->execute();
+    $curseur=$exec->fetchall();
+    return $curseur;
+}
 ?>
 
 
